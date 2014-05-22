d4067366ea64edaf9680c98d1c9c03e12e27b9f3
Michal Sliwon <m.sliwon@gmail.com>
2014-05-22 19:06:35 +0200

    what is C9 even doing

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(process.env.C9_PORT);
+app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
